a5c78cfdd1093995d3c1b940120e176fe0dc1924
soyoye olayinka <soyoyeolayinka088@gmail.com>
2026-06-02 12:11:35 +0000

    Added React frontend application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Production CI/CD Fullstack App</h1>
+
+      <p>
+        Frontend React Application Running Successfully
+      </p>
+    </div>
+  );
+}
+
+export default App;
